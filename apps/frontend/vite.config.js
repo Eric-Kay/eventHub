dde24d016a69a4aaa4f45caadd15f2a1da51845b
ERIC-KAY <avworho.eric@gmail.com>
2026-05-03 14:35:28 +0100

    Allow frontend access through dynamic ingress host

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: [
-      'ae74846921f6447e98859b18182d1e31-643303862.us-east-1.elb.amazonaws.com'
-    ]
-  }
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        allowedHosts: true
+    }
 })
